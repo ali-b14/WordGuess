474a8a18bd9456660a24b88a1d891a836e494623
ali-b14 <alib714@gmail.com>
2024-03-06 08:57:01 -0500

    Added sout line

--- src/main/java/com/github/zipcodewilmington/Main.java
+++ src/main/java/com/github/zipcodewilmington/Main.java
@@ -4,7 +4,8 @@ import javax.imageio.metadata.IIOMetadataFormatImpl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
+        System.out.println("Welcome to WordGuess lets play! ");
     }
 
 }
--- src/test/java/com/github/zipcodewilmington/WordGuessTest.java
+++ src/test/java/com/github/zipcodewilmington/WordGuessTest.java
@@ -6,6 +6,6 @@ package com.github.zipcodewilmington;
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
-public class HangmanTest {
+public class WordGuessTest {
 
 }
