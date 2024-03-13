3d189d6f812c6e1d21a1ec1de0e8357f23726e89
ali-b14 <alib714@gmail.com>
2024-03-13 15:05:47 -0400

    Finished with help

--- src/main/java/com/github/zipcodewilmington/Main.java
+++ src/main/java/com/github/zipcodewilmington/Main.java
@@ -1,23 +1,23 @@
-package com.github.zipcodewilmington;
-
-import javax.imageio.metadata.IIOMetadataFormatImpl;
-import java.util.Random;
-
-public class Main {
-        static  String pickedWord ;
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to WordGuess lets play! ");
-
-        WordGuess randomWord = new WordGuess();
-
-        String wordlist = randomWord.randomWordList();
-
-        System.out.println(randomWord.randomWordList());
-
-        pickedWord = randomWord.randomWordList();
-
-    }
-
-}
+//package com.github.zipcodewilmington;
+//
+//import javax.imageio.metadata.IIOMetadataFormatImpl;
+//import java.util.Random;
+//
+//public class Main {
+//        static  String pickedWord ;
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println("Welcome to WordGuess lets play! ");
+//
+//        WordGuess randomWord = new WordGuess();
+//
+//        String wordlist = randomWord.randomWordList();
+//
+//        System.out.println(randomWord.randomWordList());
+//
+//        pickedWord = randomWord.randomWordList();
+//
+//    }
+//
+//}
--- src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -1,6 +1,8 @@
 package com.github.zipcodewilmington;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * @author xt0fer
@@ -10,11 +12,7 @@ import java.util.Random;
 
 // Ali
 public class WordGuess {
-
-
-    public WordGuess() {
-
-    }
+    static Scanner scanner = new Scanner(System.in);
 
 
     public static void main(String[] args){
@@ -22,19 +20,13 @@ public class WordGuess {
         game.runGame();
     }
 
-
+    public WordGuess() {
+    }
     // instance variables go here
     // set up a list (array) of strings of words players could guess
-    String[] words = {"lion", "cat", "wool", "dog", "sheep", "ball", "toy", "candy", "fire", "soccer"};
 
-
-    public static void runGame(){
-//        choose a random word from a list
-        random_Num = get_Next_Random_Integer(words.length-1);
-        secret_Word = words[random_Num];
-
-
-//        while (you want to play) { //outer loop
+    private void runGame(){
+        //        while (you want to play) { //outer loop
 //            start the game
 //            set word guessed to false
 //            while (the word isn't guessed) { //inner loop
@@ -54,63 +46,129 @@ public class WordGuess {
 //        ask if player wants to play again
 //    }
 //    display game over
-    }
+        String[] wordArray = {"cat","tire","crabs","single", "randoms"};
+        
+//        choose a random word from a list
+        String secretWord = generateRandomWord(wordArray);
+        StringBuilder guesses = new StringBuilder(secretWord.length());
 
+        guesses.append("_".repeat(secretWord.length()));
+        int numberofAllowedGuesses = secretWord.length();
+        while(numberofAllowedGuesses > 0){
+            announceGame();
+            printCurrentState(guesses);
 
-    public static char printArray(char[] a){
-        for (int i = 0; i < ; i++) {
-            System.out.println();
+            char guess = getNextGuess();
+
+            if(secretWord.contains((String.valueOf(guess)))){
+                int index = secretWord.indexOf(guess);
+                guesses.setCharAt(index, guess);
+            }
+            numberofAllowedGuesses--;
+            System.out.println("Number of guesses left: " + numberofAllowedGuesses);
+            if(secretWord.equals(guesses.toString())){
+                playerWon();
+                if(!askToPlayAgain()) {
+                    break;
+                }else {
+                    secretWord = generateRandomWord(wordArray);
+                    numberofAllowedGuesses = secretWord.length();
+                    guesses = new StringBuilder(secretWord.length());
+                    guesses.append("_".repeat(secretWord.length()));
+                }
+            }
+            if(numberofAllowedGuesses <= 0) {
+                playerLost();
+                if(!askToPlayAgain()) {
+                    break;
+                }else{
+                    secretWord = generateRandomWord(wordArray);
+                    numberofAllowedGuesses = secretWord.length();
+                    guesses = new StringBuilder(secretWord.length());
+                    guesses.append("_".repeat(secretWord.length()));
+                }
+            }
         }
+
     }
 
 
-    public static void printAnnounceGame(){
+//    public static char printArray(char[] a){
+//        for (int i = 0; i < ; i++) {
+//            System.out.println();
+//        }
+//    }
+
+
+    public void announceGame(){
 //        prints a welcome
+        System.out.println("Welcome to WordGuess ");
     }
 
 
-    public static void printGameOver(){
+    public void gameOver(){
 //        prints "game over"
+        System.out.println("Game over out of guesses ");
     }
 
 
-    public static void initializeGameState(){
-//        return sets up char[] for secret word and guesses
-    }
+//    public void initializeGameState(){
+//
+////        return sets up char[] for secret word and guesses
+//    }
 
 
-    public static void getNextGuess(){
+    public char getNextGuess(){
+        System.out.println("Enter a letter: ");
+        char guess = scanner.next().charAt(0);
+        return guess;
 //        returns char from player input;
     }
 
 
-    public static void isWordGuessed(){
-//        returns booleans;
-    }
+//    public static void isWordGuessed(){
+////        returns booleans;
+//    }
 
 
-    public static void askToPlayAgain(){
+    public boolean askToPlayAgain(){
 //        returns boolean;
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Play again?\t(yes / no)");
+        String userInput = scanner1.nextLine();
+        if(userInput.equals("yes")){
+            return true;
+        }else return false;
+
     }
 
 
-    public static void printCurrentState(){
+    public void printCurrentState(StringBuilder guesses){
 //        uses printArray to show player where they are at
+        System.out.println(guesses.toString());
     }
 
 
-    public static void processLoopsThruArray(){
-//        process() loops thru the word array, looking for the inputed guess, and replaces the "_" with the guesses char if found
-    }
+//    public static void processLoopsThruArray(){
+////        process() loops thru the word array, looking for the inputted guess, and replaces the "_" with the guesses char if found
+//    }
 
 
     public static void playerWon(){
+        System.out.println("Congratulations you won! ");
 //        prints happy message
     }
 
 
     public static void playerLost(){
+        System.out.println("Unfortunately today wasn't your lucky day better luck");
 //        prints sad message
+    }
+    public String generateRandomWord (String[] words){
+        Random random = new Random();
+        int randomNumber = random.nextInt(4 + 1);
+        String magicWord = words[randomNumber];
+        return magicWord;
     }
 
 }
